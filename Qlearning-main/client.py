333b0b28a401561062391f0f0b5a469b322ab6a1
Claudino Neto <115763601+Zeromil2@users.noreply.github.com>
2024-07-15 17:09:46 -0300

    Co-authored-by: Áriston Aragão <aaa10@cin.ufpe.br>

--- Qlearning-main/client.py
+++ Qlearning-main/client.py
@@ -1,7 +1,9 @@
-#Aqui vocês irão colocar seu algoritmo de aprendizado
-
 import connection as cn
+import numpy as np
 
 s = cn.connect(2037)
 
 estado, recompensa = cn.get_state_reward(s, "jump")
+plataforma = estado[2:7] # pega do 2º em diante, pois é um binário
+sentido = estado[-2:]  # pega os dois últimos índices para o sentido
+
